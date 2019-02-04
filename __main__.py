e32395e0d1403477ba767d86e5c6f41106f4f7c3
HDJRijnders <hylbrenrijnders@hotmail.com>
2019-02-04 15:19:29 +0100

    Inital Commit!
    Added description, testing arrays, testprints

---
+++ __main__.py
@@ -0,0 +1,40 @@
+#Name: TrainFormationRandomiser
+#
+#Date: 03-02-2019 17:33
+#
+#Version: 1.0
+#
+#Description: This program randomises train formation for you after inputting a bunch of options.
+#
+#Further information: I got bored running the same train + rolling stocks all the time, this truly is Autism
+#crystallized.
+
+#array containing info
+
+    #Manufacturer, modelnr, name, era, company, length, type, power]
+testArrayLocomotives = [
+    ["Märklin", 3000, "BR 89-010", 3, "Deutsche Bundesbahn", 8, "Goods Locomotive", "Steam"],
+    ["Märklin", 3055, "NS 1212", 4, "Nederlandse Spoorwegen", 15, "Goods & Passenger Locomotive", "Electric"],
+    ["Märklin", 3324, "NS 1624", 4, "Nederlandse Spoorwegen", 17, "Goods & Passenger Locomotive", "Electric"],
+    ["Märklin", 37001, "BR 212 212-5", 3, "Technische Hilfswerke", 10, "Goods Locomotive", "Diesel-Electric"]
+]
+
+testArrayRollingstock = [
+    ["Märklin", 4070, "TEE Personenwagen ", 4, "Deutsche Bundesbahn", 20, "Passenger"],
+    ["Märklin", 4123, "Umbauwagen 2nd class", 3, "Deutsche Bundesbahn", 8, "Passenger"],
+    ["Märklin", 4124, "Umbauwagen 1 & 2nd class", 3, "Deutsche Bundesbahn", 8, "Passenger"],
+    ["Märklin", 4145, "Schwerlastwagen", 3, "Deutsche Reichsbahn(until 1945)", 8, "Goods"],
+    ["Märklin", 4169, "Stake wagon", 5, "Deutsche Bundesbahn", 5, "Goods"],
+    ["Märklin", 4164, "Prussian Landesbahn Carriage", 2, "KPEV", 8,"Passenger"],
+]
+
+#print loks
+for lok in testArrayLocomotives:
+    print(lok[2] + " Era: " + str(lok[3]))
+
+#print rollingstocks
+for rollingstock in testArrayRollingstock:
+    print(rollingstock[2] + " Era: " + str(rollingstock[3]))
+
+#GUI
+
